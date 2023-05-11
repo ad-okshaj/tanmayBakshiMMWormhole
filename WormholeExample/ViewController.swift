70a63a2382289fcc9e45c62409535ad21c0fbfad
apple <apple@apples-Mac-mini.local>
2023-05-11 16:07:36 +0530

    added code in ViewController, buttons code pending.

--- WormholeExample/ViewController.swift
+++ WormholeExample/ViewController.swift
@@ -6,17 +6,29 @@
 //
 
 import UIKit
+import MMWormhole
 
 class ViewController: UIViewController
 {
-    
-    
+    let wormhole = MMWormhole(applicationGroupIdentifier: "group.com.sclera.WormholeExample", optionalDirectory: "wormholeexample")
     @IBOutlet weak var outputLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        wormhole.listenForMessage(withIdentifier: "numberclicked")
+        {
+            (result) in
+            if let finalResult = result
+            {
+                self.display(result: (finalResult as! String))
+            }
+        }
     }
     
+    func display(result: String)
+    {
+        outputLabel.text = "New Wormhole Message: \(result)"
+    }
     
     @IBAction func button1clicked(_ sender: Any) {
     }
